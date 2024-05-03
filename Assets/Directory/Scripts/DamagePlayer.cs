d9ab63886f050d1656567d4ef8bc2b9b927fe96f
EvanVa1 <100057525+EvanVa1@users.noreply.github.com>
2024-05-03 02:23:49 -0400

    added some sounds

--- Assets/Directory/Scripts/DamagePlayer.cs
+++ Assets/Directory/Scripts/DamagePlayer.cs
@@ -6,6 +6,8 @@ using System;
 public class DamagePlayer : MonoBehaviour, IDamager
 {
     [SerializeField] private Player_SO player;
+    [SerializeField] private SFX_SO damageSound;
+    [SerializeField] private SFX_SO LaughSound;
 
     [SerializeField] float damageAmount;
 
@@ -13,18 +15,22 @@ public class DamagePlayer : MonoBehaviour, IDamager
     [SerializeField] AIBehavior1 aIBehavior1;
 
     public static Action PlayerDamagedEvent;
+
+    [SerializeField] AudioSource audioSource;
     public void DoDamage()
     {
         if(player.isTransforming)
         {
             return;
         }
+        LaughSound?.PlayRandom(audioSource);
 
         aIBehavior?.DamageAnimation();
         aIBehavior1?.DamageAnimation();
         player.health -= damageAmount;
         
         Debug.Log(player.health);
+        damageSound.Play(AudioManager.instance.effectsSource);
         PlayerDamagedEvent?.Invoke();
     }
 }
